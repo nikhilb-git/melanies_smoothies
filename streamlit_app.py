ba2b6b908fda1fc8464adcf1dd51d77a4ec63ee7
nikhilb-git <70445995+nikhilb-git@users.noreply.github.com>
2024-12-24 11:23:13 -0600

    Update streamlit_app.py
    removed the emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ cnx = st.connection("snowflake")
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:!")
+st.title("Customize Your Smoothie!")
 st.write(
     """Choose the fruits your want in your custom Smoothie!
     """)
